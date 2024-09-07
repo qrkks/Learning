65ad58c03d50bf9e46e5adf0ce7bc3bfcb0d5b6a
qrkks <qrkks3@gmail.com>
2024-09-07 09:34:57 +0800

    2024-09-07 09:34:54

---
+++ Ninja/project-vite/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+    <div :class="cls">
+        {{ msg }}
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msg: 'Hello Vue 3 + Vite',
+            cls: 'text-red-500'
+        }
+    },
+}
+</script>
