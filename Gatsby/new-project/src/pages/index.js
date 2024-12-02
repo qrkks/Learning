d435f8f647dec3a651e3f21bd13aaadc2c8e80ee
qrkks <qrkks3@gmail.com>
2024-12-02 16:10:24 +0800

    2024-12-02 16:10:23

---
+++ Gatsby/new-project/src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+function Home() {
+  return (
+    <>
+      <Layout>
+        <div>Hello World</div>
+      </Layout>
+    </>
+  )
+}
+
+export default Home
